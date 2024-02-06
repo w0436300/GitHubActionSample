e5450248a54849b5104fe7bac69de9ddf1f5b0de
Ping <144251747+w0436300@users.noreply.github.com>
2024-02-05 23:07:23 -0400

    Initial commit

---
+++ src/main/java/com/nscc/fullstk/actiondemo/ActiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.nscc.fullstk.actiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiondemoApplication.class, args);
+	}
+
+}
